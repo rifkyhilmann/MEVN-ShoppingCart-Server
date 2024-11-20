59afa5ff8f90b86a69bbd7f30af37e7db02beeb8
Rifky Hilman <rifkyhilmanudin965@gmail.com>
2024-11-20 21:42:03 +0700

    add product model

---
+++ app/models/product.model.js
@@ -0,0 +1,23 @@
+const { mongoose } = require(".");
+
+module.exports = mongoose => {
+    // Definisi schema
+    const schema = new mongoose.Schema({
+        code: String,
+        name: String,
+        price: Number,
+        description: String,
+        imageUrl: String,
+        averageRating: Number,
+    });
+
+    // Menambahkan metode toJSON
+    schema.method("toJSON", function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id; // Mengubah _id menjadi id
+        return object; // Mengembalikan objek yang telah dimodifikasi
+    });
+
+    // Membuat dan mengembalikan model
+    return mongoose.model("products", schema);
+};
